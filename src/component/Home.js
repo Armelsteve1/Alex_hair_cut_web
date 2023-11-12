f73c66ed74c7e7b764c5444eaf868bb9a1ab0525
arcel55 <arcetatchum@gmail.com>
2023-11-12 10:26:09 +0100

    home test

---
+++ src/component/Home.js
@@ -0,0 +1,20 @@
+import React  from 'react';
+
+const Home = () =>{
+    return(
+
+        <div>
+        <header>
+            <section>
+               <h1>Alex Hair Cut</h1>
+                   
+            </section>            
+        </header>
+        <body>
+
+        </body>
+    </div>
+
+    )
+}
+export default Home;
--- src/components/home.js
+++ src/components/home.js
@@ -1,15 +1,33 @@
+import "./home.css"
+import logo from "./logo1.jpg"
 const Home = ()=> {
 
     const handelContact=()=>{
         return(
             <div>contacter nous</div>
         )
-        console.alert('merci à vaous')
     }
     return(
         <div>
-            <h1>Alex hair cut</h1>
-            <h3>destination parfaite femmes, enfanst, hommes ☺️</h3>
+            
+            <section className="header">
+                <div className="header1">
+                    <p>mail : arceltatchum@gmail.com</p>
+                    <p>number : +237 697 930 445</p>
+                </div>
+                <div className="header2">
+                    {/* <a href="Home" classeName="logo" > Alex Hair Cut</a> */}
+                    <img scr={logo} alt="Alex Hai Cut" className="logo"></img>
+                    <nav className="navbar">
+                        
+                            <a href="Home">home</a>
+                            <a href="Servive">service</a>
+                            <a href="Shop">Shop</a>
+                            <a href="Pages">Pages</a>
+                       
+                    </nav>
+                </div>
+            </section>
             <button onClick={handelContact}>Contacter nous</button>
         </div>
     )
